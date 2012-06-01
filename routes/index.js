211a4f342d1f7a87bcc8ac3ab65e060f5853d368
Joe Lanman <joelanman@gmail.com>
2012-06-01 18:48:14 +0100

    performance tweaks

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var  request = require('request'),
 	 url = require('url'),
-	 $ = require('jquery'),
+	 $ = require('../lib/jquery'),
 	 jsdom = require('jsdom'),
-	 util = require('util');
+	 util = require('util'),
+	 csv = require('csv'),
+	 fs = require('fs');
 
+var postcodes = {}
+
+var getPostcodes = function(){
+
+	console.log('Getting postcodes... ');
+			
+	csv()
+	.fromPath(__dirname+'/../resources/postcodes.csv')
+	.on('data',function(data,index){
+
+		if(index === 0)
+			return;
+			
+		postcodes[data[0]] = {'easting':  Math.round(data[3]/100),
+							  'northing': Math.round(data[4]/100)};
+
+	})
+	.on('end',function(count){
+		console.log('Done getting postcodes, total: '+count);
+	})
+	.on('error',function(error){
+		console.log(error.message);
+	});
+}
+
+getPostcodes();
 
 exports.home = function(req, res){
 
@@ -61,7 +89,126 @@ exports.browse = function(req, res){
 
 };
 
+
 exports.restResults = function(req, res){
+
+	var start = Date.now();
+
+	var getDoctors = function(easting, northing){
+
+		console.log("Getting doctors: ", Date.now() - start);
+
+		url = "http://www.nhs.uk/Scorecard/Pages/Results.aspx?OrgType=1&TreatmentID=0&PageNumber=1&PageSize=0&TabId=31&SortType=1&LookupType=1&LocationType=1&SearchTerm=n44eb&DistanceFrom=5&SortByMetric=0&TrustCode=&TrustName=&DisambiguatedSearchTerm=&LookupTypeWasSwitched=False&MatchedOrganisationPostcode=&MatchedOrganisationCoords=&ServiceIDs=&ScorecardTypeCode=&NoneEnglishCountry=&HasMultipleNames=False&OriginalLookupType=1&ServiceLaunchFrom=&Filters=&TopLevelFilters=";
+		url += "&Coords="+northing +"%2c" + easting;
+
+		request({ uri: url, timeout:10000 }, function (error, response, body) {
+
+			if (error || response.statusCode !== 200) {
+				console.log('Error:' + error);
+				res.send(500);
+				return;
+			}
+
+			// get info
+			console.log("Scraping", Date.now() - start);
+			
+			var begin = body.indexOf('<div class="comparison-view">'),
+				end = body.indexOf('<div id="ctl00_PlaceHolderMain_MainContent_ctl00_divPagination"') ;
+			body = body.substring(begin, end);
+						
+			var jquery = fs.readFileSync(__dirname+'/../public/js/jquery-1.7.2.min.js').toString();
+						
+			console.log("Doc loading", Date.now() - start);
+			
+			var $body = $(body);
+			
+			console.log("body parsed", Date.now() - start);
+			
+			var $table = $body.find('#ctl00_PlaceHolderMain_MainContent_ctl00_comparisonView').parent();
+					
+			console.log("Got table", Date.now() - start);
+
+			var names = $table.find('td[headers*="gporganisationheader-0"]');
+			var addresses = $table.find('td[headers*="gpcoreaddress-1"]');
+			var GPs = $table.find('td.standard[headers*="gpgenderlanguage-6"]');
+			var feedbacks = $table.find('td.standard[headers*="gppatientfeedbackrecommend-8"]');
+			var satisfactions = $table.find('td.standard[headers*="satisfcationoverallcare-10"]');
+			var extendedAppointments = $table.find('td.standard[headers*="gpextendedappointments-15"]');
+			var patients = $table.find('td.standard[headers*="gp-registered-list-size-37"]');
+	
+			var doctors = [];
+		
+			console.log("Done scraping: ", Date.now() - start);
+
+			for(var i = 0; i<names.length; i++){
+				doctors.push({
+					"name": $(names[i]).text(),
+					"address": $.trim($(addresses[i]).text()).replace(/\n/g,"<br/>").replace(/(\+?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d?[ -]?\d?[ -]?\d)/g,"<a href=\"tel:$1\">$1</a>"),
+					"GPs": $(GPs[i]).text().replace(/Data not available/, ""),
+					"feedback": $(feedbacks[i]).text().replace(/Read\/add comments about this practice/, ""),
+					"satisfaction": $(satisfactions[i]).text(),
+					"extendedAppointments": $(extendedAppointments[i]).text(),
+					"patients": $(patients[i]).text()
+				});
+			}
+		
+			console.log("Done with controller: ", Date.now() - start);
+
+			res.render('results', {'location':postcode, 'doctors':doctors});
+					
+			/*
+			jsdom.env({
+				html: body,
+				src: [jquery],
+				done: function(err, window) {
+				
+					console.log("Doc loaded", Date.now() - start);
+					
+					var $ = window.jQuery;
+	
+					var $table = $('#ctl00_PlaceHolderMain_MainContent_ctl00_comparisonView').parent();
+				
+					console.log("Got table", Date.now() - start);
+	
+					var names = $table.find('td[headers*="gporganisationheader-0"]');
+					console.log("Got names", Date.now() - start);
+					var addresses = $table.find('td[headers*="gpcoreaddress-1"]');
+					console.log("Got adddresses", Date.now() - start);
+					var GPs = $table.find('td.standard[headers*="gpgenderlanguage-6"]');
+					console.log("Got GPs", Date.now() - start);
+					var feedbacks = $table.find('td.standard[headers*="gppatientfeedbackrecommend-8"]');
+					console.log("Got feedback", Date.now() - start);
+					var satisfactions = $table.find('td.standard[headers*="satisfcationoverallcare-10"]');
+					console.log("Got satisfaction", Date.now() - start);
+					var extendedAppointments = $table.find('td.standard[headers*="gpextendedappointments-15"]');
+					console.log("Got appointments", Date.now() - start);
+					var patients = $table.find('td.standard[headers*="gp-registered-list-size-37"]');
+					console.log("Got patients", Date.now() - start);
+			
+					var doctors = [];
+				
+					console.log("Done scraping: ", Date.now() - start);
+	
+					for(var i = 0; i<names.length; i++){
+						doctors.push({
+							"name": $(names[i]).text(),
+							"address": $.trim($(addresses[i]).text()).replace(/\n/g,"<br/>").replace(/(\+?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d?[ -]?\d?[ -]?\d)/g,"<a href=\"tel:$1\">$1</a>"),
+							"GPs": $(GPs[i]).text().replace(/Data not available/, ""),
+							"feedback": $(feedbacks[i]).text().replace(/Read\/add comments about this practice/, ""),
+							"satisfaction": $(satisfactions[i]).text(),
+							"extendedAppointments": $(extendedAppointments[i]).text(),
+							"patients": $(patients[i]).text()
+						});
+					}
+					window.close();
+				
+					console.log("Done with controller: ", Date.now() - start);
+	
+					res.render('results', {'location':postcode, 'doctors':doctors});
+				}
+			})*/
+		});
+	};
 
 	var postcode = req.params.postcode;
 	
@@ -76,78 +223,45 @@ exports.restResults = function(req, res){
 		return;
 	}
 	
-	var postcode = postcode.replace(/\-+/g," "),
-		url = "http://mapit.mysociety.org/postcode/"+ postcode.replace(/\s+/g,"");
-		
-	console.log(postcode);
-
-	request({ uri: url, timeout:10000}, function (error, response, body) {
+	var postcode = postcode.replace(/\-+/g," ");
 	
-		if (!error && response.statusCode == 200) {
+	console.log(postcode);
+	
+	if (postcodes[postcode]){
+	
+		console.log("got postcode in cache");
+	
+		var northing = postcodes[postcode].northing,
+			easting = postcodes[postcode].easting;
 			
-			body = JSON.parse(body);
-			var easting = Math.round(body.easting/100),
-				northing = Math.round(body.northing/100);
-			console.log(easting);
-			console.log(northing);
+		getDoctors(easting, northing);
 		
-			url = "http://www.nhs.uk/Scorecard/Pages/Results.aspx?OrgType=1&TreatmentID=0&PageNumber=1&PageSize=0&TabId=31&SortType=1&LookupType=1&LocationType=1&SearchTerm=n44eb&DistanceFrom=5&SortByMetric=0&TrustCode=&TrustName=&DisambiguatedSearchTerm=&LookupTypeWasSwitched=False&MatchedOrganisationPostcode=&MatchedOrganisationCoords=&ServiceIDs=&ScorecardTypeCode=&NoneEnglishCountry=&HasMultipleNames=False&OriginalLookupType=1&ServiceLaunchFrom=&Filters=&TopLevelFilters=";
-			url += "&Coords="+northing +"%2c" + easting;
+	} else {
+	
+		console.log("getting postcode co-ords...");
 		
-			request({ uri: url, timeout:10000 }, function (error, response, body) {
-
-				if (error || response.statusCode !== 200) {
-					console.log('Error:' + error);
-					res.send(500);
-					return;
-				}
-
-				jsdom.env({
-					html: body,
-					scripts: [
-					  'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'
-					]
-				}, function (err, window) {
-					var $ = window.jQuery;
+		var	url = "http://mapit.mysociety.org/postcode/"+ postcode.replace(/\s+/g,"");
+	
+		request({ uri: url, timeout:10000}, function (error, response, body) {
+	
+			if (!error && response.statusCode == 200) {
 			
-					// get info
-					console.log("scraping...");
-			
-					var names = $('[headers*="gporganisationheader-0"]'),
-						addresses = $('[headers*="gpcoreaddress-1"]'),
-						GPs = $('.standard[headers*="gpgenderlanguage-6"]'),
-						feedbacks = $('.standard[headers*="gppatientfeedbackrecommend-8"]'),
-						satisfactions = $('.standard[headers*="satisfcationoverallcare-10"]'),
-						extendedAppointments = $('.standard[headers*="gpextendedappointments-15"]'),
-						patients = $('.standard[headers*="gp-registered-list-size-37"]');
-						
-					var doctors = [];
-			
-					for(var i = 0; i<names.length; i++){
-						doctors.push({
-							"name": $(names[i]).text(),
-							"address": $.trim($(addresses[i]).text()).replace(/\n/g,"<br/>").replace(/(\+?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d[ -]?\d?[ -]?\d?[ -]?\d)/g,"<a href=\"tel:$1\">$1</a>"),
-							"GPs": $(GPs[i]).text().replace(/Data not available/, ""),
-							"feedback": $(feedbacks[i]).text().replace(/Read\/add comments about this practice/, ""),
-							"satisfaction": $(satisfactions[i]).text(),
-							"extendedAppointments": $(extendedAppointments[i]).text(),
-							"patients": $(patients[i]).text()
-						});
-					}
-					window.close();
-					res.render('results', {'location':postcode, 'doctors':doctors});
-				});
-			});
-			
-		} else {
+				body = JSON.parse(body);
+				var easting = Math.round(body.easting/100),
+					northing = Math.round(body.northing/100);
+				console.log(easting);
+				console.log(northing);
+				
+				getDoctors(easting, northing);
+				
+			} else {
 		
-			console.log("ERROR: Location not found");
-			res.send(500);
+				console.log("ERROR: Location not found");
+				res.send(500);
 			
-		}
-		
-		console.log(util.inspect(process.memoryUsage()));
-		
-	});
+			}
+		});
+				
+	};
 };
 
